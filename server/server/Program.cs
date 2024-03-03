180d12d4141ee9c65e7b2c37fbe7d26ab2219836
hrivnad <david.hrivna@seznam.cz>
2024-03-03 15:04:05 +0100

    server init

---
+++ server/server/Program.cs
@@ -0,0 +1,35 @@
+﻿
+using System.Diagnostics.CodeAnalysis;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+int buffSize = 1024;
+IPAddress ip = IPAddress.Any;
+int port = 666;
+var ipEndpoint = new IPEndPoint(ip, port);
+
+//Thread httpThread = new Thread();
+//
+//httpThread.Start();
+
+using Socket server = new Socket(ipEndpoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+server.Bind(ipEndpoint);
+
+server.Listen();
+
+var handler = await server.AcceptAsync();
+
+while(true) {
+    var buffer = new byte[buffSize];
+    var received = await server.ReceiveAsync(buffer, SocketFlags.None);
+    var response = Encoding.ASCII.GetString(buffer, 0, received);
+
+    {
+        Console.WriteLine("Received");
+        var message = "Test message";
+        var echoBytes = Encoding.ASCII.GetBytes(message);
+        await handler.SendAsync(echoBytes, 0);
+    }
+}
